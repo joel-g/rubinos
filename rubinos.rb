09df455a9d14bc88d21d49135e700632f0b0bae9
Joel Guerra <joelaguerra1@gmail.com>
2017-06-07 23:39:59 -0700

    ARGV implimented, loop implimented

--- rubinos.rb
+++ rubinos.rb
@@ -5,37 +5,48 @@ require 'open_uri_redirections'
 require 'csv'
 
 class Pizza
-	attr_reader :friend, :number, :order
-	def initialize(friend, number, order)
+	attr_reader :friend, :number, :order_time, :order_description
+	def initialize(friend, number, order, order_description)
 		@friend = friend
 		@number = number
-		@order = order
+		@order_time = order_time
+		@order_description = order_description
 	end
 end
 
-contacts = CSV.read("friends.csv")
-phone_numbers = {
-	joel_guerra: 2539613325,
-	stormy_guerra: 2539615845
-	}
-
-p contacts[0]
-
-
-# forever = true
-
-# while forever
-
-orders = []
-
-	phone_numbers.each do |friend, number|
-		orders <<	Pizza.new(friend, number, Nokogiri::HTML(open('https://order.dominos.com/orderstorage/GetTrackerData?Phone=' + number.to_s)))
-			puts "Checking #{friend}..."
-				sleep(3)
+def call_dominos(list, delay)
+	pizza_orders = []
+	list.each do |friend, number|
+		this_order = Nokogiri::HTML(open('https://order.dominos.com/orderstorage/GetTrackerData?Phone=' + number))
+		pizza_orders <<	Pizza.new(friend, number, this_order.)
+		puts "Checking #{friend}..."
+		sleep(delay)
 	end
+end
 
-puts orders[0].order.text
+
+contacts = {}
+# pizza_orders = [] 
+test_numbers = {'Joel Guerra' => '2539613325', 'Stormy Guerra' => '2539615845'}
+
+CSV.foreach("contacts.csv") do |row|
+	contacts[row[0]] = row[1]
+end
+
+if ARGV.any?
+	delay = ARGV[1].to_i || 60
+	if ARGV[0] == 'test'
+		call_dominos(test_numbers, 3)
+	elsif ARGV[0] == 'once'
+		call_dominos(contacts, delay)
+	elsif ARGV[0] == 'loop'
+		until
+			call_dominos(contacts, delay)
+		end
+	else
+		raise "Unknown Rubino's Command: #{ARGV[1]}"
+	end			
+end
 
 
-# end
-
+# puts orders[0].order.text
